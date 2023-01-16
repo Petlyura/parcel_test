1df70751b6c03f55f2358ac0570376e19224315f
Petlyura <ytymchyna@gmail.com>
2023-01-16 11:48:35 +0000

    Deploying to gh-pages from @ Petlyura/parcel_test@86cebe992ce3ddc45d74a2fba4de08d90fd3bd4e 🚀

---
+++ 01-color-switcher.df5d4b80.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]"),d=document.querySelector("body");let a=null,l=!0;e.addEventListener("click",(()=>{l?a=setInterval((()=>{d.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3):l=!1,e.disabled=!0,t.disabled=!1})),t.addEventListener("click",(()=>{clearInterval(a),l=!0,e.disabled=!1,t.disabled=!0}));
+//# sourceMappingURL=01-color-switcher.df5d4b80.js.map
